912e31c935ab7aaa7e60bcd55d0ab740a40b5094
Jared Booth <jzbooth@comcast.net>
2018-02-23 14:21:21 -0800

    removed info

--- Challenges/gameoflife/goflife.py
+++ Challenges/gameoflife/goflife.py
@@ -6,9 +6,46 @@ from copy import deepcopy
 # Initialize the app
 app = flask.Flask(__name__)
 
+
+def update_node(neighbor_sum, node):
+    if node == 1:
+        if neighbor_sum == 0:
+            return 0
+        elif neighbor_sum > 3:
+            return 0 
+        else:
+            return 1
+    else:
+        if neighbor_sum == 3:
+            return 1
+        else:
+            return 0
+        
+def update(grid):
+    M, N = grid.shape
+    new_grid = grid.copy()
+    for i in range(1,M-1):
+        for j in range(1,N-1):
+            neighbor_sum = grid[i-1:i+2,j-1:j+2].sum() - grid[i,j]
+            new_grid[i,j] = update_node(neighbor_sum, grid[i,j])
+    return new_grid
+
+def play_game(grid):
+    #for step in range(steps):
+       # print(step)
+    new_grid = update(grid)
+        #display.clear_output(wait=True)
+        #plt.imshow(new_grid, cmap='gray', interpolation='nearest')
+        
+        #plt.show()
+        #time.sleep(.1)
+    grid = new_grid
+    return grid
+
+
 @app.route("/")
 def viz_page():
-    with open("pair.html", 'r') as viz_file:
+    with open("index.html", 'r') as viz_file:
         return viz_file.read()
 
 @app.route("/gof", methods=["POST"])
@@ -19,12 +56,15 @@ def score():
     """
     data = flask.request.json
     a = data['grid']
+    print(a)
+    
+    b = list(np.zeros(len(a)))
 
     # your code for game of life goes here
     # but as first task, just set 'a' to zero
     # and send it back. See if that works.
 
-    return flask.jsonify({'grid': a})
+    return flask.jsonify({'grid': b})
 
 #--------- RUN WEB APP SERVER ------------#
 
